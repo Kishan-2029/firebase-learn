a8a935152fd8dce98d3e251a388746942708e044
kishan-2029 <kishan.prajapati@searce.com>
2023-01-12 16:07:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from './FirebaseConfig';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
